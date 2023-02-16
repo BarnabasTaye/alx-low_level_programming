08d2ca534e2f117f7f9763615caffb802a5b4b97
BarnabasTaye <Barnabasashagari@gmail.com>
2023-02-16 09:08:35 -0800

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print lines the printf function
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a peice of art,\n");
+return (0);
+}
+
