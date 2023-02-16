b96569053f5765a0e8f9f397ad579126765719b7
BarnabasTaye <Barnabasashagari@gmail.com>
2023-02-16 09:33:36 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the size of various data types on the computer it is run on
+ * Return: 0
+ */
 int main(void)
 {
-printf("Size of char: %ld byte\n", sizeof(char));
-printf("Size of short: %ld bytes\n", sizeof(short));
-printf("Size of int: %ld bytes\n", sizeof(int));
-printf("Size of long: %ld bytes\n", sizeof(long));
-printf("Size of long long: %ld bytes\n", sizeof(long long));
-printf("Size of float: %ld bytes\n", sizeof(float));
-printf("Size of double: %ld bytes\n", sizeof(double));
-printf("Size of long double: %ld bytes\n", sizeof(long double));
-printf("Size of void pointer: %ld bytes\n", sizeof(void *));
-return 0;
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
+return (0); /* Add parentheses around the expression */
 }
 
